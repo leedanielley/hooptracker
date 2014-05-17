94b793c99146dec69b24bbb90122aaf1f25a3cba
Danielle Lee <leedanielley@gmail.com>
2014-05-17 15:36:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hooptracker::Application.config.secret_key_base = '80e1b1992178af7ea54d856653527f7967e9e179bddc1dea8b06fc0a8364ff69ecacbec34dc6b8fc472b5b363d3f86e8f48de525e328623e0989eae7a3ddab98'
