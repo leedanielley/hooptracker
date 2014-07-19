9514629e51adbcbc182f26f5989474ca43ac4c18
Stephen Anderson and Danielle Lee <dev+bendycode+leedanielley@bendyworks.com>
2014-07-19 15:58:11 -0500

    add first name and last name to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+  describe "User Attributes" do
+    let(:user) { User.new first_name: 'Luna', last_name: 'Breeze', email: 'lunabreeze@hooping.org' }
+
+    subject { user }
+    its(:first_name) { should eql('Luna')}
+    its(:last_name) { should eql('Breeze')}
+    its(:email) { should eql('lunabreeze@hooping.org')}
+ 
+  end
 end
